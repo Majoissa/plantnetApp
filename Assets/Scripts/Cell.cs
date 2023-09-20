161f9c005952ad8c53e632efa0bbd1573c772026
Enric Vergara <donkikochan@gmail.com>
2023-09-20 12:10:01 +0200

    added gameManager and communication between cells and gameManager

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    public GameManager gameManger;
     public GameObject sphere;
     public GameObject cube;
     // Start is called before the first frame update
@@ -17,5 +18,20 @@ public class Cell : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    void OnMouseDown()
+    {
+        if (gameManger.isCubeTurn == true) {
+            cube.SetActive(true);
+            sphere.SetActive(false);
+            gameManger.isCubeTurn = false;
+        }
+        else
+        {
+            sphere.SetActive(true);    
+            cube.SetActive(false);
+            gameManger.isCubeTurn = true;
+        }
     }
 }
