a33ed0d7225bedbd1d5f224aea9a39a71a5ab50e
Enric Vergara <donkikochan@gmail.com>
2023-09-27 10:03:14 +0200

    added winner/draw condition on GameManager

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -46,6 +46,6 @@ public class Cell : MonoBehaviour
             gameManger.isCubeTurn = true;
         }
         
-        gameManger.check();
+        gameManger.CheckWinner();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,28 +13,66 @@ public class GameManager : MonoBehaviour
         
     }
 
-    public void check()
+    // Suponiendo que las cells se disponen de la siguiente forma:
+    // 0 | 1 | 2
+    // 3 | 4 | 5
+    // 6 | 7 | 8
+
+    public void CheckWinner()
     {
-        //Codi per comprovar si en la primera fila hi ha guanyador
-        if (cells[0].status == cells[1].status &&
-            cells[1].status == cells[2].status &&
-            cells[0].status != 0)
+        bool isDraw = true;
+
+        // Revisa las filas
+        for (int i = 0; i < 9; i += 3)
         {
-            if (cells[0].status == 1)
+            if (cells[i].status != 0 && cells[i].status == cells[i + 1].status && cells[i + 1].status == cells[i + 2].status)
             {
-                Debug.Log("Spehres han  guanyat");    
+                DeclareWinner(cells[i].status);
+                return;
             }
-            else
-            {
-                Debug.Log("Cubes han  guanyat");
-            }
-            
+            if (cells[i].status == 0 || cells[i + 1].status == 0 || cells[i + 2].status == 0) isDraw = false;
         }
+
+        // Revisa las columnas
+        for (int i = 0; i < 3; i++)
+        {
+            if (cells[i].status != 0 && cells[i].status == cells[i + 3].status && cells[i + 3].status == cells[i + 6].status)
+            {
+                DeclareWinner(cells[i].status);
+                return;
+            }
+        }
+
+        // Revisa las diagonales
+        if (cells[0].status != 0 && cells[0].status == cells[4].status && cells[4].status == cells[8].status)
+        {
+            DeclareWinner(cells[0].status);
+            return;
+        }
+
+        if (cells[2].status != 0 && cells[2].status == cells[4].status && cells[4].status == cells[6].status)
+        {
+            DeclareWinner(cells[2].status);
+            return;
+        }
+
+        // Si todas las celdas están llenas y no hay ganador, entonces es un empate.
+        if (isDraw)
+        {
+            Debug.Log("¡Es un empate!");
+        }
+    }
+
+    void DeclareWinner(int status)
+    {
+        if (status == 1)
+            Debug.Log("Spheres han ganado");
+        else
+            Debug.Log("Cubes han ganado");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
